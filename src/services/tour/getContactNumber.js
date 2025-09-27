bf484435d8989229d13dd05c45d538029d3b88c9
icfo-bookme <nationalambulancebd@gmail.com>
2025-09-27 17:08:48 +0600

    added

--- src/services/tour/getContactNumber.js
+++ src/services/tour/getContactNumber.js
@@ -1,14 +1,21 @@
 const getContactNumber = async () => {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/contact-attributes `,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/contact-attributes`,
       {
-        cache: "no-store", // Ensure no caching
+        cache: "force-cache", // Cache data for ISR
+        next: { revalidate: 60 }, // Revalidate every 60 seconds
       }
     );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data: ${res.status}`);
+    }
+
     const data = await res.json();
     return data;
   } catch (error) {
+    console.error("Error fetching contact number:", error);
     return [];
   }
 };
